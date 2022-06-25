daf37fb96dd88add9f4bc816547b02378240987d
m <m>
2022-06-25 10:15:06 +0800

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by RYAN TUNG TZE-JIN on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
